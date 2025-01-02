366cb54670dcd6ca308dd936e310b67bfcc64c25
Grizzlazy <134766687+Grizzlazy@users.noreply.github.com>
2025-01-02 20:24:42 +0700

    Run-0.6-0.5,0.3,0.1-100

--- 2_0.3.py
+++ 2_0.3.py
@@ -35,7 +35,7 @@ SET_LAST_10 = []
 BEST = []
 # 
 number_of_cities = int(os.getenv('NUMBER_OF_CITIES')) 
-delta = 0.3
+delta = 0.6
 alpha = [0.5, 0.3, 0.1]
 theta = 2
 data_set = str(os.getenv('DATA_SET'))
@@ -372,7 +372,7 @@ def Tabu_search_for_CVRP(CC):
     list_init = []
     
     start_time = time.time()
-    current_sol5 = Function.initial_solution7()
+    current_sol5 = Function.initial_solution3()
     list_init.append(current_sol5)
 
     
